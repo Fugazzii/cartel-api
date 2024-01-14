71757b26d71b1a7210c3584d9c9352a1150bf1d2
Fugazzii <sichinavailia@gmail.com>
2024-01-14 12:37:36 +0400

    Deleted unused files

--- apps/cocaines/src/core/domain/errors/exceptions/underweight.exception.ts
+++
@@ -1,8 +0,0 @@
-import { IException } from "../exception.interface";
-import ErrorMessage from "../messages";
-
-export class UnderWeightProductException extends IException {
-    public constructor() {
-        super(ErrorMessage.UnderWeightProduct);
-    }
-}
--- apps/cocaines/src/core/domain/errors/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./messages";
-export * from "./exception.interface";
-
-export * from "./exceptions/overweight.exception";
-export * from "./exceptions/underweight.exception";

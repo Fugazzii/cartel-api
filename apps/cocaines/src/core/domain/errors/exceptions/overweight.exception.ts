3f646d3186a03657fbccb2a11cf6e82c0afd8103
Fugazzii <sichinavailia@gmail.com>
2024-01-14 12:37:36 +0400

    Deleted unused files

--- apps/cocaines/src/core/domain/errors/exceptions/overweight.exception.ts
+++
@@ -1,8 +0,0 @@
-import { IException } from "../exception.interface";
-import ErrorMessage from "../messages";
-
-export class OverweightProductException extends IException {
-    public constructor() {
-        super(ErrorMessage.OverWeightProduct);
-    }
-}
--- apps/cocaines/src/core/domain/errors/messages/index.ts
+++
@@ -1,9 +0,0 @@
-const OverWeightProduct = "Product weight exceeds limit";
-const UnderWeightProduct = "Product weight is below limit";
-
-const ErrorMessages = {
-    OverWeightProduct,
-    UnderWeightProduct
-};
-
-export default ErrorMessages;
